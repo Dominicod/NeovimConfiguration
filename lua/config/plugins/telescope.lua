95459c7ba0474e213961edb005c5d91e6758ff4e
Dominic O'Donnell <dodonnell@honehealth.com>
2025-07-18 15:54:59 -0500

    Update grep

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -60,7 +60,9 @@ return {
           hidden = true,
         }
       end, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sg', function()
+        builtin.live_grep { additional_args = { '--fixed-strings', '--ignore-case' } }
+      end, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find open files' })
