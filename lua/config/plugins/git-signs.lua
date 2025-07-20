ed8b4fdb2519638c8d774c2f8632f9c8ab1c37f9
Dominicod <dominicodonnell99@gmail.com>
2025-07-20 17:26:26 -0500

    Git changes

--- lua/config/plugins/git-signs.lua
+++ lua/config/plugins/git-signs.lua
@@ -1,6 +1,9 @@
 return {
   {
     'lewis6991/gitsigns.nvim',
+    dependencies = {
+      'tpope/vim-fugitive',
+    },
     opts = {
       on_attach = function(bufnr)
         local gitsigns = require 'gitsigns'
@@ -29,7 +32,20 @@ return {
         end, { desc = 'Jump to previous [G]it change' })
 
         map('n', '<leader>gb', gitsigns.blame, { desc = '[G]it [B]lame' })
-        map('n', '<leader>gd', gitsigns.diffthis, { desc = '[G]it [D]iff against index' })
+        map('n', '<leader>gd', function()
+          gitsigns.diffthis '~1'
+        end, { desc = '[G]it [D]iff against last commit' })
+        map('n', '<leader>gd', function()
+          gitsigns.diffthis '~'
+        end, { desc = '[G]it [D]iff against head' })
+        map('n', '<leader>gs', gitsigns.stage_hunk, { desc = '[G]it [S]tage hunk' })
+        map('n', '<leader>gr', gitsigns.reset_hunk, { desc = '[G]it [R]eset hunk' })
+        map('n', '<leader>gS', gitsigns.stage_buffer, { desc = '[G]it [S]tage buffer' })
+        map('n', '<leader>gR', gitsigns.reset_buffer, { desc = '[G]it [R]eset buffer' })
+        gitsigns.setup {
+          signcolumn = true,
+          numhl = true,
+        }
       end,
     },
   },
